cf659682216261cab3c2672b7f29210a9428debf
AidaG <a.garzonbe@gmail.com>
2021-06-18 10:48:33 -0500

    Version inicial

---
+++ src/App.js
@@ -0,0 +1,45 @@
+
+import './App.css';
+import React from "react";
+import Formulario from './pages/formulario/Formulario';
+import Nineras from './pages/nineras/Nineras';
+import Home from './pages/home/Home';
+import Error from './pages/error/Error';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Switch>
+            <Route path='/formulario'sensitive>
+              <Formulario/>
+            </Route>
+
+            <Route path='/nineras'sensitive>
+              <Nineras/>
+            </Route>
+
+            <Route exact path='/'>
+              <Home/>
+            </Route>
+
+            <Route path='*'>
+              <Error/>
+            </Route>
+
+          </Switch>
+        </div>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
